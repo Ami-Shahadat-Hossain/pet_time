55158d9df8b20df8ef6aadcf9a68a68b0d333558
Ami-Shahadat-Hossain <shahadat.user@gmail.com>
2025-01-12 16:16:19 +0600

    users table migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,22 +19,51 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->string('f_name')->nullable();
+            $table->string('l_name')->nullable();
+            $table->string('phone', 25)->nullable();
+            $table->string('image', 30)->default('def.png');
+            $table->string('street_address', 250)->nullable();
+            $table->string('country', 50)->nullable();
+            $table->string('city', 50)->nullable();
+            $table->string('zip', 20)->nullable();
+            $table->string('house_no', 50)->nullable();
+            $table->string('apartment_no', 50)->nullable();
+            $table->string('cm_firebase_token', 191)->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->string('payment_card_last_four', 191)->nullable();
+            $table->string('payment_card_brand', 191)->nullable();
+            $table->text('payment_card_fawry_token')->nullable();
+            $table->string('login_medium', 191)->nullable();
+            $table->string('social_id', 191)->nullable();
+            $table->boolean('is_phone_verified')->default(false);
+            $table->string('temporary_token', 191)->nullable();
+            $table->boolean('is_email_verified')->default(false);
+            $table->double('wallet_balance', 8, 2)->nullable();
+            $table->double('loyalty_point', 8, 2)->nullable();
+            $table->tinyInteger('login_hit_count')->default(0);
+            $table->boolean('is_temp_blocked')->default(false);
+            $table->timestamp('temp_block_time')->nullable();
+            $table->string('referral_code', 255)->nullable();
+            $table->foreignId('referred_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->string('app_language', 191)->default('en');
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
+        // Schema::create('password_reset_tokens', function (Blueprint $table) {
+        //     $table->string('email')->primary();
+        //     $table->string('token');
+        //     $table->timestamp('created_at')->nullable();
+        // });
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
+        // Schema::create('sessions', function (Blueprint $table) {
+        //     $table->string('id')->primary();
+        //     $table->foreignId('user_id')->nullable()->index();
+        //     $table->string('ip_address', 45)->nullable();
+        //     $table->text('user_agent')->nullable();
+        //     $table->longText('payload');
+        //     $table->integer('last_activity')->index();
+        // });
     }
 
     /**
@@ -43,7 +72,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        // Schema::dropIfExists('password_reset_tokens');
+        // Schema::dropIfExists('sessions');
     }
 };
